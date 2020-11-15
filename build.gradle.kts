d84027bb670505c5edef8ad368f88e6853c9a220
felipe_c_rodrigues <felipe.cavalcante.rod@gmail.com>
2020-11-15 00:38:59 -0300

    transactions integrated tests.

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,9 @@ dependencies {
 	// JSON Web Token
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 
+	//Google GSON
+	implementation("com.google.code.gson:gson:2.8.6")
+
 	//MySQL Driver
 	runtimeOnly("mysql:mysql-connector-java")
 
@@ -50,6 +53,8 @@ dependencies {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
 	}
 	testImplementation("org.springframework.security:spring-security-test")
+	testImplementation(platform("org.junit:junit-bom:5.7.0"))
+	testImplementation("org.junit.jupiter:junit-jupiter")
 }
 
 tasks.withType<Test> {
