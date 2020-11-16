d713bf744702e7f3ed888ae9d4ac578ce013dd24
Breno Marino de Souza Ferreira <breno_marino@hotmail.com>
2020-11-16 19:39:55 -0300

    wip: ajuste StudentDTO

--- src/main/kotlin/br/com/fiap/transactions/dto/StudentDTO.kt
+++ src/main/kotlin/br/com/fiap/transactions/dto/StudentDTO.kt
@@ -1,9 +1,19 @@
 package br.com.fiap.transactions.dto
 
 import br.com.fiap.transactions.entity.Student
+import java.math.BigInteger
+import javax.persistence.Column
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
 
 data class StudentDTO(
-        val name: String
+    val id: BigInteger,
+    val name: String,
+    val className: String,
+    val cpf: String,
+    val addresId: String,
+    val cardId: String
 ){
-    constructor(student: Student):this(""){}
+    constructor():this(BigInteger.ONE, "","","","","")
+    constructor(student: Student):this(BigInteger.ONE, "","","","",""){}
 }
